f3fe8c349592f311f8a04b6f7d05b437104fda48
arurua913 <71250122+arurua913@users.noreply.github.com>
2020-11-16 14:59:52 +0800

    Add files via upload

---
+++ 王心如-week10-隨堂練習.py
@@ -0,0 +1,57 @@
+#1.變數賦值
+x=1
+
+## 用等號=讓電腦給儲存空間
+## 等號左邊
+## 等號右邊
+
+## 有甚麼用處?
+
+# 2. 數字資料+數學運算
+## 運算子 七種方式 // % **
+## 函數
+
+x=2
+y=5
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x**y)
+z=x*y
+print(z)
+
+magic_number=22**3+17//5+17%5
+print(magic_number)
+
+
+# 3. 電腦輸入
+## input("Hint")函數
+### 文字資料
+
+num = input("Enter a number?")
+num = int(num)
+num = num + 1
+print(num)
+
+
+num = (int(input("Enter a number?"))+1)
+num = num + 1
+print(num)
+
+
+num = int(input("Enter a number?"))+1
+print(num)
+
+
+print(int(input("Enter a number"))+1)
+
+
+
+# 4.電腦輸出
+## print()函數
+print("my magic number is", magic_number)
+
+
